48aebca39b4f810f8bafd4c37301dbfbb819e8a8
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-09-01 11:56:24 +0530

    model updated

--- lib/screen/no_internet_screen.dart
+++ lib/screen/no_internet_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
 class NoInternetScreen extends StatelessWidget {
-  const NoInternetScreen({super.key});
+  final Widget? child;
+  const NoInternetScreen({Key? key, this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Image.asset(
-          "packages/internet_handler/assets/no-internet.png",
-          width: 400,
-          height: 400,
-        ),
+        child: child ??
+            Image.asset(
+              "packages/internet_handler/assets/no-internet.png",
+              width: 400,
+              height: 400,
+            ),
       ),
     );
   }
